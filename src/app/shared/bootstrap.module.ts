37e39c931e952e8740aef57dc186875a0516854f
Cristina Sabau <cristina.sabau@wirtek.com>
2020-03-07 16:25:23 +0200

    Add ngx-bootstrap and separate module

---
+++ src/app/shared/bootstrap.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ButtonsModule } from 'ngx-bootstrap';
+
+@NgModule({
+    imports: [
+        ButtonsModule
+    ],
+    exports: [
+        ButtonsModule
+    ],
+})
+export class BootstrapModule { }
